d827ae4f391e945f8a96ca1c91266eee749562ad
Filip Bergander <filip.bergander1996@gmail.com>
2026-04-20 10:42:21 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
